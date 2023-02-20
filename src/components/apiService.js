859f49128247dfeb89296061805b1c1d2c6e1766
Of-Nameless <79377789+Of-Nameless@users.noreply.github.com>
2023-02-20 17:54:58 +0100

    init

---
+++ src/components/apiService.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const ENDPOINT = 'https://pixabay.com/api';
+const API_KEY = '33721850-c6fdd091d70b8ca25b72e0c80';
+const OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true';
+
+export default class ApiService {
+  constructor() {
+    this.query = '';
+    this.page = 1;
+    this.per_page = 40;
+  }
+
+  async getImage() {
+    try {
+      const url = await axios.get(
+        `${ENDPOINT}/?key=${API_KEY}&q=${this.query}&${OPTIONS}&per_page=${this.per_page}&page=${this.page}`
+      );
+
+      this.nextPage();
+        return url.data;
+        
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  nextPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,138 @@
+import {Notify} from 'notiflix';
+import ApiService from './components/apiService.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import throttle from 'lodash.throttle'
+
+const form = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+const apiService = new ApiService();
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+// let page = null;
+// let limitPerPage = 0;
+
+form.addEventListener('submit', onSubmit);
+window.addEventListener('scroll', throttle(infinityScroll, 500))
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const value = form.elements.searchQuery.value.trim();
+  // if (value === '') {
+  //   return
+  // }
+
+  apiService.resetPage();
+  clearImages();
+
+  apiService.query = value;
+
+  fetchArticles().finally(() => form.reset());
+}
+
+async function fetchArticles() {
+  try {
+    const data = await apiService.getImage();
+    console.log(data);
+    const hits = data.hits;
+    console.log(hits);
+    const totalHits = data.totalHits;
+    console.log(totalHits);
+
+    if (hits.length === 0) {
+      throw new Error(
+      Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+    )
+      )
+    };
+
+    createMarkup(hits);
+    lightbox.refresh();    
+
+    let page = apiService.page - 1;
+    let limitPerPage = apiService.per_page;
+    if (apiService.page - 1 === 1) {
+      Notify.success(`Hooray! We found ${totalHits} images.`)
+    };
+    
+    const totalPages = totalHits / limitPerPage;
+    if (page > totalPages) {
+      throw new Error(
+        Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+        )
+      )
+    };
+
+  } catch (err) {
+    return err;
+  }
+
+}
+
+function clearImages() {
+  gallery.innerHTML = '';
+}
+
+function createMarkup(arr) {
+  const markup = arr.map(({
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads
+  }) => {
+    return `
+  <div class="container">
+    <div class="images">
+    <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy"  /></a>
+    </div>
+    <div class="article">
+    <h3>
+      <b>Likes:</b>
+      <b>${likes}</b>
+    </h3>
+    <h3>
+      <b>Views:</b>
+      <b>${views}</b>
+    </h3>
+    <h3>
+      <b>Comments:</b>
+      <b>${comments}</b>
+    </h3>
+    <h3>
+      <b>Downloads</b>
+      <b>${downloads}</b>
+    </h3>
+  </div>
+</div>`
+  }).join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup)
+};
+
+function smoothScroll() {
+  const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
+};
+
+function infinityScroll() {
+  const documentRect = document.documentElement.getBoundingClientRect();
+  if (documentRect.bottom < document.documentElement.clientHeight + 150) {
+    page++;
+    fetchArticles()
+  }
+}
