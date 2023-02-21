9947fcc90c2bfc32149f898e2453b2d069c0dc1a
Of-Nameless <79377789+Of-Nameless@users.noreply.github.com>
2023-02-21 15:36:44 +0100

    info

--- src/index.js
+++ src/index.js
@@ -70,8 +70,8 @@ async function fetchArticles() {
     const totalPages = totalHits / limitPerPage;
     if (page > totalPages) {
       throw new Error(
-        Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
+        Notify.info(
+      "We're sorry, but you've reached the end of search results."
         )
       )
     }
